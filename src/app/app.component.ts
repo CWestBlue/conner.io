24c6f6e1d254b1a3e04bf672776c5678fa314966
Conner <cwestblox@gmail.com>
2017-08-09 16:23:15 -0500

    added some stuff to my about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,28 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   @HostListener('window:resize') onResize() {
-    this.picSize(document.getElementById('my-image'));
+    this.setToScreenHeight(document.getElementById('my-image'));
+    this.setToScreenWidth(document.getElementById('my-image'));
+    this.setToScreenHeight(document.getElementById('About'));
+    this.setToScreenHeight(document.getElementById('Contact'));
   }
   ngOnInit() {
-    this.picSize(document.getElementById('my-image'));
+    this.setToScreenHeight(document.getElementById('my-image'));
+    this.setToScreenWidth(document.getElementById('my-image'));
+    this.setToScreenHeight(document.getElementById('About'));
+    this.setToScreenHeight(document.getElementById('Contact'));
 
   }
 
-  picSize(myImage) {
+  setToScreenHeight(element) {
     const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    element.style.minHeight = String(windowHeight) + 'px';
+    element.style.maxHeight = String(windowHeight) + 'px';
+  }
+
+  setToScreenWidth(element) {
     const windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    myImage.style.minHeight = String(windowHeight) + 'px';
-    myImage.style.maxHeight = String(windowHeight) + 'px';
-    myImage.style.minWidth = String(windowWidth) + 'px';
-    myImage.style.maxWidth = String(windowWidth) + 'px';
+    element.style.minWidth = String(windowWidth) + 'px';
+    element.style.maxWidth = String(windowWidth) + 'px';
   }
 }
